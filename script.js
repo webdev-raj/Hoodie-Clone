54a5d9e83cc3f68ccb537015396920ac01b4654c
webdev-raj <codewithsapnap@gmail.com>
2025-01-01 15:05:38 +0530

    minimal change

--- script.js
+++ script.js
@@ -14,4 +14,10 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
   freeMode: true,
+  breakpoints: {
+    320: { slidesPerView: 1, spaceBetween: 10 },
+    480: { slidesPerView: 2, spaceBetween: 20 },
+    768: { slidesPerView: 3, spaceBetween: 30 },
+    1024: { slidesPerView: 3.2, spaceBetween: 40 },
+  },
 });
